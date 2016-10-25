ca41828aab35de24825aeb31e8662a942b84c4db
Alexandre Huber <alexandre.huber@outlook.com>
2016-10-25 09:46:32 +0200

    Draft

---
+++ run_analysis.R
@@ -0,0 +1,90 @@
+require(dplyr)
+require(readr)
+
+
+# The read.dataset function reads and assembles the data subset defined by the
+# name argument (which corresponds to the data subset folder and file names) and
+# returns the loaded data.
+#   Arguments:  - name: character variable defining the name of the data subset
+#               - activity.labels: character vector defining the names of the
+#                   activities
+#               - features: character vector defining the names of the columns
+#   Output: dplyr data table containing the data subset properly annotated
+#               with the following columns:
+#               - subject: test subject number
+#               - subset: subset name (= name argument)
+#               - activity: associated activity as defined by the
+#                    activity.labels variable
+#               - parameter variables
+read.dataset <- function(name, activity.labels, features) {
+    
+    data <- read_fwf(paste0("raw/", name, "/X_", name, ".txt"),
+                     col_positions = fwf_widths(rep(16, 561), col_names = features))
+    subject <- read.csv(paste0("raw/", name, "/subject_", name, ".txt"),
+                        as.is = T, header = F, col.names = "Subject")
+    subset <- sub(pattern = "^(.)", replacement = "\\U\\1", tolower(name), perl = T)
+    subject$Subset <- rep(subset, length(subject$Subject))
+    activity <- tbl_df(read.csv(paste0("raw/", name, "/y_", name, ".txt"), 
+                                header = F, col.names = c("ID")))
+    activity <- select(left_join(activity, activity.labels), Activity)
+
+    data <- tbl_df(cbind(cbind(subject, activity), data))
+    data
+}
+
+# Setting the working directory to the dataset path (unquote and adapt as needed)
+#setwd("Coursera\\GACDw4\\")
+
+# Loading and cleaning / capitalizing activity labels
+activity.labels <- read.csv("raw/activity_labels.txt", sep = " ", as.is = T,
+                            header = F, col.names = c("ID", "Activity"))
+activity.labels$Activity <- gsub("_", " ", activity.labels$Activity) %>%
+    tolower() %>%
+    sub(pattern = "^(.)", replacement = "\\U\\1", perl = T) %>%
+    factor()
+
+# Loading and cleaning / capitalizing feature names
+features <- read.csv("raw/features.txt", sep = " ", as.is = T, header = F)
+features <- features$V2 %>%
+    sub(pattern = "\\bt", replacement = "Time") %>%
+    sub(pattern = "\\bf", replacement = "Frequency") %>%
+    sub(pattern = "\\b([a-z])", replacement = "\\U\\1", perl = T) %>%
+    sub(pattern = "[,-]([a-z])", replacement = "\\U\\1", perl = T) %>%
+    sub(pattern = "Gyro", replacement = "Gyroscope") %>%
+    sub(pattern = "Mag", replacement = "Magnitude") %>%
+    sub(pattern = "Mad", replacement = "MeanAbsoluteDeviation") %>%
+    sub(pattern = "Acc", replacement = "Acceleration") %>%
+    sub(pattern = "Std", replacement = "StandardDeviation") %>%
+    sub(pattern = "Max", replacement = "Maximum") %>%
+    sub(pattern = "Min", replacement = "Minimum") %>%
+    sub(pattern = "MeanFreq", replacement = "MeanFrequency") %>%
+    sub(pattern = "Sma", replacement = "SignalMagnitudeArea") %>%
+    sub(pattern = "MaxInds", replacement = "MaximalIndex") %>%
+    sub(pattern = "Iqr", replacement = "InterquartileRange") %>%
+    sub(pattern = "\\(\\)", replacement = "") %>%
+    sub(pattern = "([[:digit:]]+),([[:digit:]]+)", replacement = "\\1To\\2") %>%
+    gsub(pattern = "[(),-]", replacement = "")
+
+# Loading, naming and assembling the train dataset with the read.dataset function
+train <- read.dataset(name = "train", activity.labels = activity.labels, features = features)
+# Loading, naming and assembling the test dataset with the read.dataset function
+test <- read.dataset(name = "test", activity.labels = activity.labels, features = features)
+
+# Fusing test and train datasets and conversion to dplyr data table
+d <- tbl_df(rbind(train, test))
+# Converting the Subject and Subset (train/test) columns to factors
+d$Subject <- factor(d$Subject)
+d$Subset <- factor(d$Subset)
+
+# Selecting only means and standard deviations in addition to factor variables
+d <- d[c(names(d)[1:3], names(d)[grepl("(Mean|StandardDeviation)[X-Z]?$", names(d))])]
+
+write.csv(d, file = "HumanActivityRecognition.csv")
+
+# Calculating means of each variable by Subject, Subset and Activity
+#   grouping by Subset does not break the data in more groups since subjects
+#   do not belong to more than 1 subset.
+means <- group_by(d, Subject, Subset, Activity) %>%
+    summarise_all(mean)
+
+write.csv(means, file = "HumanActivityRecognitionSummary.csv")
